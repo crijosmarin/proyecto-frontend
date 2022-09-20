bcb0d3111f421bd20a2dbb3dd5311609acaf0ab8
Cristian Marin <crijosar@gmail.com>
2022-09-20 14:09:53 -0500

    se agrego funciones al boton de reinicio y se desabilita los botones de ataque al llegar a 0 vidas

--- juego mokepon/js/mokepon.js
+++ juego mokepon/js/mokepon.js
@@ -15,6 +15,9 @@ function iniciarJuego() {
     botonAgua.addEventListener('click', ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 function seleccionarMascotaJugador() {
@@ -127,6 +130,17 @@ function crearMensajeFinal(resultadoFinal) {
     parrafo.innerHTML = resultadoFinal
 
     sectionMensajes.appendChild(parrafo)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+}
+
+function reiniciarJuego() {
+  location.reload()
 }
 
 function aleatorio(min, max) {
