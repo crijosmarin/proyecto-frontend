67dfe324652f7a3e2916bfd173c110e1e5bf2479
Cristian Marin <crijosar@gmail.com>
2022-09-20 10:06:18 -0500

    se agrego la funcion aleatorio para seleccionar la mascota del enemigo con la funcion seleccionarMascotaEnemigo

--- juego mokepon/js/mokepon.js
+++ juego mokepon/js/mokepon.js
@@ -7,19 +7,40 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if (inputHipodoge.checked) {
-        alert("Haz seleccionado a Hipodoge 🐋");
+        spanMascotaJugador.innerHTML = "Hipodoge"
       } 
       else if (inputCapipepo.checked) {
-        alert("Haz seleccionado a Capipepo 🐦");
+        spanMascotaJugador.innerHTML = "Capipepo"
       } 
       else if (inputRatigueya.checked) {
-        alert("Haz seleccionado a Ratigueya 🐭");
+        spanMascotaJugador.innerHTML = "Ratigueya"
       }
       else {
         alert("Ninguna mascota seleccionada")
       }
+
+      seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 3)
+  let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+  if (ataqueAleatorio == 1) {
+      spanMascotaEnemigo.innerHTML = 'Hipodoge'
+  } else if (ataqueAleatorio == 2) {
+      spanMascotaEnemigo.innerHTML = 'Capipepo'
+  } else {
+      spanMascotaEnemigo.innerHTML = 'Ratigueya'
+  }
+  
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', iniciarJuego)
